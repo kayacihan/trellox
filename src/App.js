568f4a2606c3a9d3748f4ded3214cb25e13ddf1f
Cihan Kaya <cihankaya42@gmail.com>
2021-05-15 14:32:20 +0300

    Column template created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import Column from "./components/Column";
+
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">Trellox</div>
-        <p>
-        hello
-        </p>
+      <Column />
     </div>
   );
 }
---
+++ src/components/Column.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Column extends Component {
+    render() {
+        return (
+            <div className="Column">
+                <p> Column1  </p >
+                <p> Column2  </p >
+                <p> Column3  </p >
+            </div>
+        )
+    }
+}
+
+export default Column
